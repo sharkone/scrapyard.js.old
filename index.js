2e30f87cb4d5584f5f5a1cab142d99baf1c52e35
Thomas Texier <sharkone@en-mousse.org>
2015-09-30 01:52:24 +0000

    Added cache

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require('newrelic');
 
 // ----------------------------------------------------------------------------
 
+var VERSION = '0.0.1';
+
+// ----------------------------------------------------------------------------
+
 var async            = require('async');
+var cache            = require('express-redis-cache')({ client: require('redis').createClient(process.env.REDISCLOUD_URL, { no_ready_check: true }) });
 var express          = require('express');
 var stringCapitalize = require('string-capitalize');
 var traktApi         = require('trakt-api');
@@ -32,107 +37,105 @@ var traktApi         = require('trakt-api');
 
 // ----------------------------------------------------------------------------
 
+cache.on('message', function (message) {
+  console.log(message);
+});
+
+// ----------------------------------------------------------------------------
+
 var app = express();
 
 app.set('json spaces', 2);
 
 app.get('/', function(req, res) {
-  res.send('');
+  res.json({ version: VERSION });
 });
 
-var host   = process.env.IP || '0.0.0.0'
-var port   = process.env.PORT || 8080;
-var server = app.listen(port, host, function() {
-  console.log('[scrapyard] Starting on %s:%s', host, port);
+var server = app.listen(process.env.PORT || 8080, process.env.IP || '0.0.0.0', function() {
+  console.log('[scrapyard] Starting on %s:%s', server.address().address, server.address().port);
 });
 
 function shutdown() {
   console.log('[scrapyard] Stopping');
-  process.exit();
+  server.close(process.exit);
 }
 
-process.on('SIGINT', function() {
-  shutdown();
-});
-
-process.on('SIGTERM', function() {
-  shutdown();
-});
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
 
 // ----------------------------------------------------------------------------
 
-var trakt = traktApi('64cf92c702ff753cc14a6d421824efcd32f22058f79bf6d637fa92e23229f35f', { 'logLevel': 'info'});
+var trakt = traktApi('64cf92c702ff753cc14a6d421824efcd32f22058f79bf6d637fa92e23229f35f', { logLevel: 'info'});
 
 // ----------------------------------------------------------------------------
 // Movies
 // ----------------------------------------------------------------------------
 
 function getMovieInfo(traktSlug, peopleNeeded, callback) {
-  trakt.movie(traktSlug, { 'extended': 'full,images' }, function(err, movieData) {
+  trakt.movie(traktSlug, { extended: 'full,images' }, function(err, movieData) {
     if (err)
       callback(err);
     else {
       var movieInfo = {
-        'trakt_slug':     movieData['ids']['slug'],
-        'imdb_id':        movieData['ids']['imdb'],
-        'title':          movieData['title'],
-        'year':           movieData['year'],
-        'overview':       movieData['overview'],
-        'tagline':        movieData['tagline'],
-        'thumb':          movieData['images']['poster']['full'],
-        'art':            movieData['images']['fanart']['full'],
-        'runtime':        (parseInt(movieData['runtime']) * 60 * 1000) || 0,
-        'genres':         movieData['genres'].map(function(x) { return stringCapitalize(x); }),
-        'rating':         movieData['rating'],
-        'released':       movieData['released'],
-        'certification':  movieData['certification']
+        trakt_slug:     movieData.ids.slug,
+        imdb_id:        movieData.ids.imdb,
+        title:          movieData.title,
+        year:           movieData.year,
+        overview:       movieData.overview,
+        tagline:        movieData.tagline,
+        thumb:          movieData.images.poster.full,
+        art:            movieData.images.fanart.full,
+        runtime:        (parseInt(movieData.runtime, 10) * 60 * 1000) || 0,
+        genres:         movieData.genres.map(function(x) { return stringCapitalize(x); }),
+        rating:         movieData.rating,
+        released:       movieData.released,
+        certification:  movieData.certification
       };
 
       if (peopleNeeded) {
-        trakt.moviePeople(traktSlug, { 'extended': 'images' }, function(err, peopleData) {
+        trakt.moviePeople(traktSlug, { extended: 'images' }, function(err, peopleData) {
           if (err)
             callback(err);
           else {
-            movieInfo['cast'] = [];
+            movieInfo.cast = [];
             if ('cast' in peopleData)
-              for (var i = 0; i < peopleData['cast'].length; i++) {
-                movieInfo['cast'].push({
-                  'name':       peopleData['cast'][i]['person']['name'],
-                  'headshot':   peopleData['cast'][i]['person']['images']['headshot']['full'],
-                  'character':  peopleData['cast'][i]['character']
+              for (var i = 0; i < peopleData.cast.length; i++) {
+                movieInfo.cast.push({
+                  name:       peopleData.cast[i].person.name,
+                  headshot:   peopleData.cast[i].person.images.headshot.full,
+                  character:  peopleData.cast[i].character
                 });
             }
 
-            movieInfo['crew'] = { 'directing': [], 'production': [], 'writing': [] };
+            movieInfo.crew = { directing: [], production: [], writing: [] };
             if ('crew' in peopleData) {
-              if ('directing' in peopleData['crew'])
-                for (var i = 0; i < peopleData['crew']['directing'].length; i++) {
-                  movieInfo['crew']['directing'].push({
-                    'name':     peopleData['crew']['directing'][i]['person']['name'],
-                    'headshot': peopleData['crew']['directing'][i]['person']['images']['headshot']['full'],
-                    'job':      peopleData['crew']['directing'][i]['job']
+              if ('directing' in peopleData.crew)
+                for (var i = 0; i < peopleData.crew.directing.length; i++) {
+                  movieInfo.crew.directing.push({
+                    name:     peopleData.crew.directing[i].person.name,
+                    headshot: peopleData.crew.directing[i].person.images.headshot.full,
+                    job:      peopleData.crew.directing[i].job
                   });
                 }
 
-              if ('production' in peopleData['crew'])
-                for (var i = 0; i < peopleData['crew']['production'].length; i++) {
-                  movieInfo['crew']['production'].push({
-                    'name':     peopleData['crew']['production'][i]['person']['name'],
-                    'headshot': peopleData['crew']['production'][i]['person']['images']['headshot']['full'],
-                    'job':      peopleData['crew']['production'][i]['job']
+              if ('production' in peopleData.crew)
+                for (var i = 0; i < peopleData.crew.production.length; i++) {
+                  movieInfo.crew.production.push({
+                    name:     peopleData.crew.production[i].person.name,
+                    headshot: peopleData.crew.production[i].person.images.headshot.full,
+                    job:      peopleData.crew.production[i].job
                   });
                 }
 
-              if ('writing' in peopleData['crew'])
-                for (var i = 0; i < peopleData['crew']['writing'].length; i++) {
-                  movieInfo['crew']['writing'].push({
-                    'name':     peopleData['crew']['writing'][i]['person']['name'],
-                    'headshot': peopleData['crew']['writing'][i]['person']['images']['headshot']['full'],
-                    'job':      peopleData['crew']['writing'][i]['job']
+              if ('writing' in peopleData.crew)
+                for (var i = 0; i < peopleData.crew.writing.length; i++) {
+                  movieInfo.crew.writing.push({
+                    name:     peopleData.crew.writing[i].person.name,
+                    headshot: peopleData.crew.writing[i].person.images.headshot.full,
+                    job:      peopleData.crew.writing[i].job
                   });
                 }
             }
-
             callback(null, movieInfo);
           }
         });
@@ -146,16 +149,15 @@ function getMovieInfo(traktSlug, peopleNeeded, callback) {
 function parseMovieListData(movieListData, callback) {
   async.map(movieListData,
   function(movieData, callback) {
-    var traktSlug = ('movie' in movieData) ? movieData['movie']['ids']['slug'] : movieData['ids']['slug'];
-    getMovieInfo(traktSlug, false, callback);
+    getMovieInfo(('movie' in movieData) ? movieData.movie.ids.slug : movieData.ids.slug, false, callback);
   },
   callback);
 }
 
-app.get('/api/movies/popular', function(req, res) {
-  var page = parseInt(req.query.page) || 1;
+app.get('/api/movies/popular', cache.route({ expire: 3600 }), function(req, res) {
+  var page = parseInt(req.query.page, 10) || 1;
 
-  trakt.moviePopular({ 'page': page, 'limit': 31 }, function(err, movieListData) {
+  trakt.moviePopular({ page: page, limit: 31 }, function(err, movieListData) {
     if (err)
       res.sendStatus(err['statusCode']);
     else
@@ -163,15 +165,15 @@ app.get('/api/movies/popular', function(req, res) {
         if (err)
             res.sendStatus(err['statusCode']);
           else
-            res.send({ 'movies': movieInfos });
+            res.json({ 'movies': movieInfos });
       });
   });
 });
 
-app.get('/api/movies/trending', function(req, res) {
-  var page = parseInt(req.query.page) || 1;
+app.get('/api/movies/trending', cache.route({ expire: 3600 }), function(req, res) {
+  var page = parseInt(req.query.page, 10) || 1;
 
-  trakt.movieTrending({ 'page': page, 'limit': 31 }, function(err, movieListData) {
+  trakt.movieTrending({ page: page, limit: 31 }, function(err, movieListData) {
     if (err)
       res.sendStatus(err['statusCode']);
     else
@@ -179,12 +181,12 @@ app.get('/api/movies/trending', function(req, res) {
         if (err)
             res.sendStatus(err['statusCode']);
           else
-            res.send({ 'movies': movieInfos });
+            res.json({ 'movies': movieInfos });
       });
   });
 });
 
-app.get('/api/movies/search', function(req, res) {
+app.get('/api/movies/search', cache.route({ expire: 3600 }), function(req, res) {
   var query = req.query.query || '';
 
   trakt.searchMovie(query, function(err, movieListData) {
@@ -195,38 +197,38 @@ app.get('/api/movies/search', function(req, res) {
         if (err)
             res.sendStatus(err['statusCode']);
           else
-            res.send({ 'movies': movieInfos });
+            res.json({ movies: movieInfos });
       });
   });
 });
 
-app.get('/api/movie/:trakt_slug', function(req, res) {
+app.get('/api/movie/:trakt_slug', cache.route({ expire: 3600 }), function(req, res) {
   getMovieInfo(req.params.trakt_slug, true, function(err, movieInfo) {
     if (err)
       res.sendStatus(err['statusCode']);
     else
-      res.send(movieInfo);
+      res.json(movieInfo);
   });
 });
 
-// ----------------------------------------------------------------------------
-// Shows
-// ----------------------------------------------------------------------------
+// // ----------------------------------------------------------------------------
+// // Shows
+// // ----------------------------------------------------------------------------
 
-app.get('/api/shows/popular', function(req, res) {
-  var page = parseInt(req.query.page) || 1;
-  //res.send(page.toString());
-});
+// app.get('/api/shows/popular', function(req, res) {
+//   var page = parseInt(req.query.page) || 1;
+//   //res.json(page.toString());
+// });
 
-app.get('/api/shows/trending', function(req, res) {
-  var page = parseInt(req.query.page) || 1;
-  //res.send(page.toString());
-});
+// app.get('/api/shows/trending', function(req, res) {
+//   var page = parseInt(req.query.page) || 1;
+//   //res.json(page.toString());
+// });
 
-app.get('/api/shows/search', function(req, res) {
-  var query = req.query.query || '';
-  if (query)
-    res.send(query);
-  else
-    res.sendStatus(404);
-});
+// app.get('/api/shows/search', function(req, res) {
+//   var query = req.query.query || '';
+//   if (query)
+//     res.json(query);
+//   else
+//     res.sendStatus(404);
+// });
